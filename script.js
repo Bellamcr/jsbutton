db6043ab7cafdfb5a5dfb2e5fe3148e90139349c
Bellamcr <izabella.mcr@gmail.com>
2023-10-27 19:36:14 -0600

    changes on script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("👋 🌎");
+const root = document.getElementById("root");
+console.log(root);
+function handleClick (e) {
+    console.dir(e)
+}
+const newButton = document.createElement("button");
+root.appendChild(newButton);
+newButton.innerText = 'X';
+newButton.classList.add("square");
+
+newButton.setAttribute("onclick", "handleClick(this)")
+
+
